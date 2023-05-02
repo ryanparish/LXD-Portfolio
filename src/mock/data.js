3d6a4900f54f2c0f8fe1a682a4e8b5642630eba5
Ryan Parish <33233330+ryanparish@users.noreply.github.com>
2023-05-02 09:37:38 -0500

    Update data.js
    updated

--- src/mock/data.js
+++ src/mock/data.js
@@ -28,8 +28,7 @@ export const aboutData = {
   paragraphOne: "Hey there! I'm Ryan. I have been designing and developing Learning Experiences for 15 years.",
   paragraphTwo: "I approach each project holistically-- asking whether the problem at hand is best addressed through a learning intervention, process change, or by getting people in a room to hash things out.",
   paragraphThree: "I believe that everyone has the potential for greatness, but that potential is often limited by fear, an attempt to hide weaknesses, and being unwilling to ask for help.",
-  paragraphFour: "I understand how to remove those barriers-- empowering teams to live up to their true potential.",
-  paragraphFour: "I've spent the past few years putting those lessons into practice, leading and developing a team of seven direct and indirect reports into a high-performing, collaborative force.",
+  paragraphFour: "I understand how to remove those barriers-- empowering teams to live up to their true potential. I've spent the past few years putting those lessons into practice, leading and developing a team of seven direct and indirect reports into a high-performing, collaborative force.",
   resume: 'https://resume.io/r/VLu9fOOQd',
 }; 
 
