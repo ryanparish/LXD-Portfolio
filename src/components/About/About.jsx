68923c522adfe4e62efa44148b3460abd7f70656
Ryan Parish <33233330+ryanparish@users.noreply.github.com>
2023-05-02 12:30:11 -0500

    fix format

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
               </div>
             </Fade>
           </Col>
-          <Col md={7} sm={14}>
+          <Col md={6} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
